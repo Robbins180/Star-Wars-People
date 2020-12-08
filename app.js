3c035b8087cadba9b90f4ee24dfdf83fa616b6cc
Drew R <drewr@Drews-MacBook-Pro.local>
2020-12-08 16:34:29 -0600

    project1

---
+++ app.js
@@ -0,0 +1,23 @@
+$(() => {
+
+
+
+  // $.ajax({
+  //   // url: 'https://swapi.dev/api/people/',
+  //
+  //   // data: {
+  //     // "$limit" : userInput || 10 }
+  //
+  // }).then(
+  //     (data) => {
+  //       console.log(data);
+  //
+  //
+  //
+  //     })
+
+
+
+
+
+  })
