1cd116d20cf13093bc466b3c1abf5372b9b2bd74
Drew R <drewr@Drews-MacBook-Pro.local>
2020-12-09 07:51:13 -0600

    project1

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Star-wars-memory
-A memory game for star wars charcters and etc.
+# Star-wars-People-modal
+
+Star Wars People Modal is an app made for searching for different characters/people in the star wars universe.
+
+Click the START button on the main screen to begin.
+
+On the next box, Enter a number between 1 and 83 followed but the '/' symbol to search for specific traits about the character.
+
+You can always enter another number in the same box for a different result.
+
+Once you are done, click the CLOSE button on the top of the box to return to the main screen.
+
+Happy Searching!
--- app.js
+++ app.js
@@ -1,23 +1,92 @@
 $(() => {
 
+  const $modal = $('#modal')
+
+  const startBtn = () => {
+    $modal.css('display', 'block')
+  }
+
+  const closeBtn = () => {
+    $modal.css('display', 'none')
+  }
+
+  const $openButton = $('#startBtn')
+  $openButton.on('click', startBtn)
+
+  const $closeButton = $('#close')
+  $closeButton.on('click', closeBtn)
 
 
-  // $.ajax({
-  //   // url: 'https://swapi.dev/api/people/',
-  //
-  //   // data: {
-  //     // "$limit" : userInput || 10 }
-  //
-  // }).then(
-  //     (data) => {
-  //       console.log(data);
-  //
-  //
-  //
-  //     })
+  $('form').on('click', (event) => {
+
+    event.preventDefault()
+
+    const userInput = $('input[type="text"]').val()
+
+    $.ajax({
+    url: 'https://swapi.dev/api/people/' + userInput,
 
 
-
-
-
+    }).then(
+      (data) => {
+        $('#name').html(data.name)
+        $('#height').html(data.height)
+        $('#mass').html(data.mass)
+        $('#hair_color').html(data.hair_color)
+        $('#gender').html(data.gender)
+        console.log(data);
+      },
+      () => {
+        console.log('bad request');
+      }
+    )
   })
+})
+
+// Break from People to Films
+
+
+// $(() => {
+//
+//   const $modal2 = $('#modal2')
+//
+//   const openModal2 = () => {
+//     $modal2.css('display', 'block')
+//   }
+//
+//   const closeModal2 = () => {
+//     $modal2.css('display', 'none')
+//   }
+//
+//   const $openButton2 = $('#openModal2')
+//   $openButton2.on('click', openModal2)
+//
+//   const $closeButton2 = $('#close2')
+//   $closeButton2.on('click', closeModal2)
+//
+//
+//   $('#filmes').on('click', (event) => {
+//
+//     event.preventDefault()
+//
+//     const userInput = $('input[type="text"]').val()
+//
+//     $.ajax({
+//     url: 'https://swapi.dev/api/films/' + userInput,
+//
+//
+//     }).then(
+//       (data) => {
+//         $('#title').html(data.title)
+//         $('#episode_id').html(data.episode_id)
+//         $('#director').html(data.director)
+//         $('#producer').html(data.producer)
+//         $('#release_date').html(data.release_date)
+//         console.log(data);
+//       },
+//       () => {
+//         console.log('bad request');
+//       }
+//     )
+//   })
+// })
